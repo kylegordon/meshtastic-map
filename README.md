a2d71e7aef11d108d7ff028760156137097cc1d8
Kyle Gordon <kyle@lodge.glasgownet.com>
2021-09-18 12:05:43 +0100

    In-browser and running

--- README.md
+++ README.md
@@ -4,6 +4,20 @@ To test this, do something like:
 while true; do mosquitto_pub -h 172.24.32.13 -t 'mgdm/geo' -m '{"lat": 55.9,"lon": -4.3,"label":"hello"}'; sleep 2; done
 ```
 
-Notes
+### MQTT ###
 
-Leaflet market balloons - https://rikdeboer.medium.com/leaflet-maps-marker-fun-games-53d81fdd2f52
+The 'local' eclipse-mosquitto instance bridges the TCP based MQTT protocol at mqtt.meshtastic.org to a WebSockets enabled server running locally
+
+### Notes ###
+
+Leaflet marker balloons - https://rikdeboer.medium.com/leaflet-maps-marker-fun-games-53d81fdd2f52
+Leaflet intro https://maptimeboston.github.io/leaflet-intro/
+
+
+### Example Message ###
+
+From 'mesh' device with MQTT enabled
+msh/1/json/LongSlow/!336aa961/POSITION_APP {"channelId":"LongSlow","gatewayId":"!336aa961","packet":{"from":862628193,"to":862621573,"channel":177,"decoded":{"latitudeI":557808280,"longitudeI":-44715801,"altitude":208,"batteryLevel":100,"time":1631537932},"id":153724265,"rxTime":1631537932,"rxSnr":0.0,"hopLimit":3,"wantAck":false,"priority":"RELIABLE","rxRssi":0}}
+
+From 'mesh' device without MQTT enabled
+msh/1/json/LongSlow/!336a8f85/POSITION_APP {"channelId":"LongSlow","gatewayId":"!a84c7038","packet":{"from":862621573,"to":-1,"channel":177,"decoded":{"latitudeI":557805567,"longitudeI":-44715839,"altitude":270,"batteryLevel":30,"time":1631537986},"id":1489549959,"rxTime":1631537953,"rxSnr":11.25,"hopLimit":1,"wantAck":false,"priority":"UNSET","rxRssi":0}}
--- app/app.js
+++ app/app.js
@@ -1,29 +1,60 @@
 const map = L.map("map", {
     center: [55.9, -4.3],
-    zoom: 13
+    zoom: 7
 });
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
-    attribution:
-        '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 }).addTo(map);
 
-const client = new Paho.MQTT.Client("172.24.32.13", 9001, "clientId");
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//     maxZoom: 19,
+//     attribution:
+//         '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>',
+// }).addTo(map);
+
+const client = new Paho.MQTT.Client("localhost", 9001, "clientId");
 client.onMessageArrived = onMessage;
 client.onConnectionLost = onConnectionLost;
 
 function onConnect() {
-    client.subscribe('mgdm/geo');
+    client.subscribe('msh/1/json/LongSlow/+/POSITION_APP');
 };
 
 function onMessage(message) {
     console.log("Message received");
-    const m = JSON.parse(message.payloadString);
-    console.log(m);
 
-    const marker = L.marker([m["lat"], m["lon"]]).addTo(map);
-    marker.bindPopup(m["label"]);
+    // try {
+        const ParsedMessage = JSON.parse(message.payloadString);
+        console.log(ParsedMessage);
+    // } catch (e) {
+    //     return undefined;
+    //     console.log("Failed to parse message");
+    // }
+
+    console.log("Parsing results");
+    channelId = ParsedMessage["channelId"];
+    gatewayId = ParsedMessage["gatewayId"];
+    packet = ParsedMessage["packet"];
+
+    // How to derive lat/lon documented at https://github.com/meshtastic/Meshtastic-protobufs/blob/master/mesh.proto#L40
+    latitude = packet['decoded']['latitudeI']  / 10000000;
+    longitude = packet['decoded']['longitudeI']  / 10000000;
+    altitude = packet['decoded']['altitude'];
+    console.log("Device at " + latitude + ", " + longitude + ". Altitude: " + altitude);
+
+    if ( latitude !== 0 && longitude !== 0 ) {
+        const marker = L.marker([latitude, longitude]).addTo(map);
+        // marker.bindPopup(ParsedMessage["label"]);
+    } else {
+        console.log("Shipment for Null Island");
+    }
+
+
 };
 
 function onConnectionLost(responseObject) {
